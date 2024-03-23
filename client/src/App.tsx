e82c27d732db3fb50c296991b3ff9c458d13dd0a
Dolly <abcddolly1234@gmail.com>
2024-03-23 14:54:03 +0530

    tailwind and shadcn/ui init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <div>Evento frontend</div>;
+  return (
+    <div className="text-red-500">
+      Evento frontend
+      <Button> Hello</Button>
+    </div>
+  );
 }
 
 export default App;
