59ffaf35f537b7b3052659dbf5204fc7ce422be5
Dolly <abcddolly1234@gmail.com>
2024-03-24 19:20:25 +0530

    added footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex items-center justify-between flex-col gap-4 p-5 text-center sm:flex-row ">
+        <p className="text-xl">Eventrologo</p>
+        <p>2024 Eventro. All rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
