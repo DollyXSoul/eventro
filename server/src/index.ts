12b6175928745b596c645097d52459d3e92c9780
Dolly <abcddolly1234@gmail.com>
2024-03-28 18:08:01 +0530

    Setting up GraphQL server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,40 @@
-const express = require("express");
-import { Request, Response, Express } from "express";
+import express from "express";
 const dotenv = require("dotenv");
-
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Evento server");
-});
+  //use middleware for parsing json
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  app.use(express.json());
+
+  const gqlserver = new ApolloServer({
+    typeDefs: `
+    type Query {
+      hello : String
+    }`,
+    resolvers: {
+      Query: {
+        hello: () => "Hi from Graphql server",
+      },
+    },
+  });
+
+  await gqlserver.start();
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
+
+  app.use("/graphql", expressMiddleware(gqlserver));
+
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
+
+init();
